953761e67f630030d460c951f2175052b015449e
dhaval2208 <dhavalbhimani2000@gmail.com>
2022-11-30 20:33:48 -0500

    layout

---
+++ app/src/main/java/com/dhaval/contacts/MainActivity.kt
@@ -0,0 +1,16 @@
+// Name :- Dhaval Bhimani Zaverbhai
+// Student id :- A00255187
+// Course :- JAV-1001 - 91337 - App Development for Android - 202209 - 001
+
+
+package com.dhaval.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
