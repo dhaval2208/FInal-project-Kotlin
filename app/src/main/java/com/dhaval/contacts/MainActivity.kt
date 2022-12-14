b83ed7a85a06b3cc9e8c279000c457d339e6263a
dhaval2208 <dhavalbhimani2000@gmail.com>
2022-12-14 16:15:30 -0500

    add to first and last name

--- app/src/main/java/com/dhaval/contacts/MainActivity.kt
+++ app/src/main/java/com/dhaval/contacts/MainActivity.kt
@@ -25,16 +25,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.dhaval.contacts.databinding.ActivityMainBinding
+import javax.sql.CommonDataSource
 
 class MainActivity : AppCompatActivity() {
      private lateinit var binding: ActivityMainBinding
-
+//TAG
      private val TAG = "CONTACT_ADD_TAG"
-
+// Array permisssion
     private lateinit var contactPermissions: Array<String>
-
+// request for Write contacts permission
     private val WRITE_CONTACT_PERMISSION_CODE =100
-
+// request for image pick gallery
     private val IMAGE_PICK_GALLERY_CODE =200
 
     private var image_uri: Uri? = null
@@ -44,13 +45,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+// init permission array to write contacts
         contactPermissions = arrayOf(Manifest.permission.WRITE_CONTACTS)
-
+// pick image from gallery
         binding.profile.setOnClickListener{
             openGalleryIntent()
         }
-
+// click to save contacts
         binding.SaveNumber.setOnClickListener{
           if(isWriteContactPermissionEnable()){
               saveContact()
@@ -62,20 +63,38 @@ class MainActivity : AppCompatActivity() {
     }
 private fun saveContact(){
     Log.d(TAG, "saveContact")
-
+// define variables
     val firstname = binding.FirstName.text.trim()
     val lastname = binding.LastName.text.trim()
     val phonemobile = binding.PhoneMobile.text.trim()
     val phonehome = binding.PhoneHome.text.trim()
     val email = binding.Email.text.trim()
     val address = binding.Address.text.trim()
-
+// input data
     Log.d(TAG, "SaveContact: FirstName $firstname")
     Log.d(TAG, "SaveContact: Last Name $lastname")
     Log.d(TAG, "SaveContact: Phone Mobile $phonemobile")
     Log.d(TAG, "SaveContact: Phone Home $phonehome")
     Log.d(TAG, "SaveContact: Email $email")
     Log.d(TAG, "SaveContact: Address $address")
+// init array of object ContentProviderOperation
+    val Contacts = ArrayList<ContentProviderOperation>()
+// add to contact id
+    val rawContactId = Contacts.size
+    Contacts.add(ContentProviderOperation.newInsert(
+        ContactsContract.RawContacts.CONTENT_URI)
+        .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE , null)
+        .withValue(ContactsContract.RawContacts.ACCOUNT_NAME , null)
+        .build())
+// add to first name and last name
+    Contacts.add(ContentProviderOperation.newInsert(
+        ContactsContract.Data.CONTENT_URI)
+        .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+        .withValue(ContactsContract.RawContacts.Data.MIMETYPE,ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+        .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, firstname)
+        .withValue(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME, lastname)
+        .build())
+
 
 }
 
@@ -86,6 +105,8 @@ private fun saveContact(){
     private fun requestWriteContactPermission(){
         ActivityCompat.requestPermissions(this , contactPermissions, WRITE_CONTACT_PERMISSION_CODE)
     }
+
+    // pick image from gallery
     private fun openGalleryIntent() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE)
@@ -97,13 +118,16 @@ private fun saveContact(){
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // handle permission request result
         if (grantResults.isNotEmpty()) {
             if (requestCode == WRITE_CONTACT_PERMISSION_CODE) {
-                val haveWriteContactPermission = grantResults[0] == PackageManager.PERMISSION_DENIED
+                val haveWriteContactPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED
                 if(haveWriteContactPermission){
+                    // permission granted save contact
                     saveContact()
                 }
                 else{
+                    // permission denied , can't save contact
                     Toast.makeText(this, "Permission deneid" , Toast.LENGTH_SHORT).show()
                 }
             }
@@ -112,13 +136,14 @@ private fun saveContact(){
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+// handle image pick result
         if (resultCode == RESULT_OK){
             if (requestCode == IMAGE_PICK_GALLERY_CODE){
                 image_uri = data!!.data
                 binding.profile.setImageURI(image_uri)
             }
         }
+        // canclled
         else{
             Toast.makeText(this, "cancelled" , Toast.LENGTH_SHORT).show()
         }
